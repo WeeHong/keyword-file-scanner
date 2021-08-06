2eece1bc3de1ebff2986f37e541ad46e0ee9199e
Wee Hong KOH <weehongayden.developer@hotmail.com>
2021-08-06 23:30:09 +0800

    feat: add .ignorefolder functionality

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"bufio"
 	"fmt"
 	"io/ioutil"
@@ -11,40 +12,132 @@ import (
 	"strings"
 )
 
-func main() {
-	scanDirectory()
+type folder struct {
+	List map[string]bool
 }
 
-func scanDirectory() {
-	err := filepath.Walk(".",
+type myCloser interface {
+	Close() error
+}
+
+// closeFile is a helper function which streamlines closing
+// with error checking on different file types.
+func closeFile(f myCloser) {
+	err := f.Close()
+	check(err)
+}
+
+// readAll is a wrapper function for ioutil.ReadAll. It accepts a zip.File as
+// its parameter, opens it, reads its content and returns it as a byte slice.
+func readAll(file *zip.File) []byte {
+	fc, err := file.Open()
+	check(err)
+	defer closeFile(fc)
+
+	content, err := ioutil.ReadAll(fc)
+	check(err)
+
+	return content
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readFile(file *os.File) {
+	scanner := bufio.NewScanner(file)
+	collection := make(map[string]bool)
+
+	for scanner.Scan() {
+		key := strings.Trim(scanner.Text(), " ")
+		if _, ok := collection[key]; !ok {
+			collection[key] = true
+		}
+	}
+
+	ignoredFolder.List = collection
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var ignoredFolder folder
+
+func init() {
+	if _, err := os.Stat(".folderignore"); err == nil {
+		file, err := os.Open(".folderignore")
+		if err != nil {
+			log.Fatalf("Failed to open file: %v", err)
+		}
+		defer file.Close()
+		readFile(file)
+	}
+}
+
+func main() {
+	// zipFile := "./compress/aws-sdk-go-main.zip"
+	// zf, err := zip.OpenReader(zipFile)
+	// check(err)
+	// defer closeFile(zf)
+
+	// for _, file := range zf.File {
+	// 	fmt.Printf("=%s\n", file.Name)
+	// 	// fmt.Printf("%s\n\n", readAll(file)) // file content
+	// }
+	fmt.Println(ignoredFolder)
+	_ = scanDirectory()
+	// fmt.Println(filePaths)
+
+	// keywords := os.Args[1:]
+	// for _, keyword := range keywords {
+	// 	fmt.Println(keyword)
+	// }
+}
+
+func scanDirectory() []string {
+	var p []string
+	_, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Unable to allocate root path.")
+	}
+	err = filepath.Walk(".",
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
+			}
+			if info.Mode().IsRegular() {
+				p = append(p, path)
 			}
 			return nil
 		})
 	if err != nil {
 		log.Println(err)
 	}
+	return p
 }
 
-func filterDirectory(path string) {
+func filterDirectory(path string, keyword string) {
 	fi, err := os.Stat(path)
 	if err != nil {
 		log.Fatalf("Unable to read file from the path: %v", err)
 	}
 	if fi.Mode().IsRegular() {
-		fmt.Println("file", path)
 		f, err := ioutil.ReadFile(path)
 		if err != nil {
 			log.Fatalf("Unable to read file content: %v", err)
 		}
 
-		matched, err := regexp.Match(`Unable`, f)
+		matched, err := regexp.Match(keyword, f)
 		if err != nil {
 			log.Fatalf("Unable to match the regexp pattern")
 		}
-		fmt.Println(matched)
+
+		if matched {
+			fmt.Println("File path: ", path)
+		}
 	}
 }
 
@@ -75,11 +168,10 @@ func showLineContainsText(path string) {
 	}
 	defer f.Close()
 
-	// Splits on newlines by default.
 	scanner := bufio.NewScanner(f)
 
 	line := 1
-	// https://golang.org/pkg/bufio/#Scanner.Scan
+
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 		if strings.Contains(scanner.Text(), "Unable") {
